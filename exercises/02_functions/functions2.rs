12c3a9713a325dabc13a1d56eac0df4eb1aff6b0
Daniel <13129650+pereiradaniel@users.noreply.github.com>
2024-12-13 14:41:47 -0500

    Completed up to function3

--- exercises/02_functions/functions2.rs
+++ exercises/02_functions/functions2.rs
@@ -1,10 +1,11 @@
 // TODO: Add the missing type of the argument `num` after the colon `:`.
-fn call_me(num:) {
+fn call_me(num: i32) {
     for i in 0..num {
         println!("Ring! Call number {}", i + 1);
     }
 }
 
 fn main() {
-    call_me(3);
+    let num3: i32 = 10;
+    call_me(num3);
 }
